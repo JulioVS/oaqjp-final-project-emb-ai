37ac44b68edf31d47a638f18c748da9906556101
JulioVS <julioer@me.com>
2024-08-05 14:28:53 -0400

    Ran static code analysis - End Project

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+'''Emotion Web Server'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,15 +6,17 @@ app = Flask("Emotion Detection")
 
 @app.route("/")
 def home():
+    '''Render homepage'''
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def emo_detect():
+    '''Call Emotion API'''
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
 
-    if response['dominant_emotion'] == None:
+    if response['dominant_emotion'] is None:
         return " Invalid text! Please try again!"
 
     formatted_output = f"For the given statement, the system response is \
@@ -27,5 +30,5 @@ def emo_detect():
     return formatted_output
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
     
