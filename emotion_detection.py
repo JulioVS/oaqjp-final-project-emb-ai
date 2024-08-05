96a104bb8682c8593d46d6461becf9e928a2cc82
JulioVS <julioer@me.com>
2024-08-05 11:28:41 -0400

    Add dominant emotion and scores JSON output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,5 +7,18 @@ def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json=input_json, headers=header)
+    
+    formatted_response = json.loads(response.text)
+    scores = formatted_response['emotionPredictions'][0]['emotion']
+    dominant = max(scores, key=scores.get)
 
-    return response.text
+    json_output = {
+        'anger': scores['anger'],
+        'disgust': scores['disgust'],
+        'fear': scores['fear'],
+        'joy': scores['joy'],
+        'sadness': scores['sadness'],
+        'dominant_emotion': dominant
+    }
+
+    return json_output
